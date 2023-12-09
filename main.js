f329fd50df7095e93b54d231c2e9484000e0f813
Martin33 <martintroncoso43@gmail.com>
2023-12-08 22:13:08 -0300

    desactiveDB

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Uso "type": "module", ES6 usar modulos import y export
 import app from "./src/app.js"
-import {sequelize} from "./src/database/database.js"
+//import {sequelize} from "./src/database/database.js"
 import  "./src/database/models/CardsModels.js"
 import {} from "dotenv/config.js"
 const PORT = process.env.PORT
@@ -8,8 +8,8 @@ console.log(process.env.PORT)
 
 async function main () {
         try {
-            await sequelize.sync({alter:false}) // force: recrea tablas, alter: altera si es necesario
-            console.log("soy sequelize")
+            // await sequelize.sync({alter:false}) // force: recrea tablas, alter: altera si es necesario
+            // console.log("soy sequelize")
             app.listen(PORT,() => {
                 console.log("hola soy appexpress en puerto: " + PORT)
             })
